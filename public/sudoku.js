19754e167ea2d21f64e36442513a93e686af3461
Brian Hanson <brianmnhanson@gmail.com>
2018-03-17 21:08:56 -0500

    Remove unnecessary ;

--- public/sudoku.js
+++ public/sudoku.js
@@ -1,16 +1,16 @@
 jQuery.fn.extend({
 	disable: function (state) {
 		return this.each(function () {
-			var $this = jQuery(this);
+			var $this = jQuery(this)
 			if ($this.is('input, button'))
-				this.disabled = state;
+				this.disabled = state
 			else if ($this.is('select') && state)
-				$this.attr('disabled', 'disabled');
+				$this.attr('disabled', 'disabled')
 			else if ($this.is('select') && !state)
-				$this.removeAttr('disabled');
+				$this.removeAttr('disabled')
 			else
-				$this.toggleClass('disabled', state);
-		});
+				$this.toggleClass('disabled', state)
+		})
 	}
 });
 $(document).ready(
@@ -24,20 +24,20 @@ $(document).ready(
 		var undo = [];
 
 		function setMode(m) {
-			$('button, #items, #setup, #choices, #entry, a').hide();
-			$('#menu').css("grid-area", "");
+			$('button, #items, #setup, #choices, #entry, a').hide()
+			$('#menu').css("grid-area", "")
 			switch (m) {
 				case 'edit':
-					$('#setup, #choices, #entry, #new, #solve, #dectitle, #inctitle, #list, #delete').show();
-					$('#delete').disable(!("sk " + title_t.value) in localStorage);
-					$("#title").disable(false);
-					$("div > div").css({ color: "red", background: "" });
+					$('#setup, #choices, #entry, #new, #solve, #dectitle, #inctitle, #list, #delete').show()
+					$('#delete').disable(!("sk " + title_t.value) in localStorage)
+					$("#title").disable(false)
+					$("div > div").css({ color: "red", background: "" })
 					$("div > div").each(function (i) {
-						$(this).text(theSudoku.value[i] == 0 ? ' ' : theSudoku.value[i]);
-					});
-					change_digit(null);
-					clean_url();
-					break;
+						$(this).text(theSudoku.value[i] == 0 ? ' ' : theSudoku.value[i])
+					})
+					change_digit(null)
+					clean_url()
+					break
 				case 'play':
 					$('#setup, #choices, #entry, #new, #solved, #edit, #save, #clear, #list, #undo, a').show()
 					$("#title, #undo").disable(true)
@@ -52,66 +52,66 @@ $(document).ready(
 					check_guess()
 					updateLink()
 					undo = []
-					break;
+					break
 				case 'list':
-					$('#new, #solve, #edit, #items').show();
-					$('#menu').css("grid-area", "name");
-					show_hide(hide);
-					build_list();
-					clean_url();
-					break;
+					$('#new, #solve, #edit, #items').show()
+					$('#menu').css("grid-area", "name")
+					show_hide(hide)
+					build_list()
+					clean_url()
+					break
 			}
-			mode = m;
+			mode = m
 		}
 
 		function clean_url() {
 			if (document.URL.indexOf("?") > 0) {
-				window.history.replaceState('', '', document.URL.substring(0, document.URL.indexOf("?")));
+				window.history.replaceState('', '', document.URL.substring(0, document.URL.indexOf("?")))
 			}
 		}
 
 		function show_hide(h) {
-			hide = h;
+			hide = h
 			if (hide) {
-				$("tr").has(":contains('✓')").hide();
-				$("#hide").hide();
-				$("#show").show();
+				$("tr").has(":contains('✓')").hide()
+				$("#hide").hide()
+				$("#show").show()
 
 			} else {
-				$("tr").show();
-				$("#hide").show();
-				$("#show").hide();
+				$("tr").show()
+				$("#hide").show()
+				$("#show").hide()
 			}
 		}
 
 		function as_string(value, solved) {
-			return value.join('') + (solved ? 'Y' : 'N');
+			return value.join('') + (solved ? 'Y' : 'N')
 		}
 
 		function from_string(s) {
-			return s.substr(0, 81).replace(/ /g, '0').split('').map(c => parseInt(c));
+			return s.substr(0, 81).replace(/ /g, '0').split('').map(c => parseInt(c))
 		}
 
-		var dayOfWeek = "Sunday Monday Tuesday Wednesday Thursday Friday Saturday".split(" ");
+		var dayOfWeek = "Sunday Monday Tuesday Wednesday Thursday Friday Saturday".split(" ")
 		function updateLink() {
 			var href = document.URL.split("?")[0] + "?" +
-				encodeURI(theSudoku.name) + "&" + as_string(theSudoku.value, false);
-			var mail_a = document.getElementById("mail");
+				encodeURI(theSudoku.name) + "&" + as_string(theSudoku.value, false)
+			var mail_a = document.getElementById("mail")
 			if (mail_a != null) {
-				var v = /\d+-\d+-\d+$/g.exec(theSudoku.name);
-				var d = v ? new Date(v[0]).getUTCDay() : new Date().getDay();
-				var day = dayOfWeek[d];
-				var iOS = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
-				var mail = iOS ? "googlegmail:///co" : "mailto:";
+				var v = /\d+-\d+-\d+$/g.exec(theSudoku.name)
+				var d = v ? new Date(v[0]).getUTCDay() : new Date().getDay()
+				var day = dayOfWeek[d]
+				var iOS = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform)
+				var mail = iOS ? "googlegmail:///co" : "mailto:"
 				mail_a.href = mail + "?"
 					+ "subject=" + encodeURIComponent(day + "'s sudoku")
-					+ "&body=" + encodeURIComponent(href);
+					+ "&body=" + encodeURIComponent(href)
 			}
 		}
 
 		function save() {
-			theSudoku.name = title_t.value;
-			theSudoku.valid = true;
+			theSudoku.name = title_t.value
+			theSudoku.valid = true
 			localStorage["sudoku"] = JSON.stringify(theSudoku, 0, 1)
 		}
 
@@ -138,131 +138,128 @@ $(document).ready(
 		}
 
 		function inc_title(n) {
-			var v = /\d+-\d+-\d+$/g.exec(title_t.value);
+			var v = /\d+-\d+-\d+$/g.exec(title_t.value)
 			if (v) {
-				var d = new Date(v[0]);
-				d.setTime(d.getTime() + n * 24 * 3600000);
-				title_t.value = "STrib " + d.toISOString().slice(0, 10);
-				check_title();
-				return;
+				var d = new Date(v[0])
+				d.setTime(d.getTime() + n * 24 * 3600000)
+				title_t.value = "STrib " + d.toISOString().slice(0, 10)
+				check_title()
+				return
 			}
-			v = /\d+$/g.exec(title_t.value);
+			v = /\d+$/g.exec(title_t.value)
 			if (v) {
-				var num = parseInt(v[0]) + n;
-				var str = num.toFixed(0);
+				var num = parseInt(v[0]) + n
+				var str = num.toFixed(0)
 				while (str.length < v[0].length) {
-					str = "0" + str;
+					str = "0" + str
 				}
-				title_t.value = title_t.value.substring(0, v.index) + str;
-				check_title();
+				title_t.value = title_t.value.substring(0, v.index) + str
+				check_title()
 				return;
 			}
 		}
 
 		function get_row(n) {
-			var value = Array(10).fill(0);
-			var s = Math.floor(s / 3) * 3 * 9 + (n % 3) * 3;
+			var value = Array(10).fill(0)
+			var s = Math.floor(s / 3) * 3 * 9 + (n % 3) * 3
 			for (i = 0; i < 3; i++) {
 				for (j = 0; j < 3; j++) {
-					value[theSudoku.guess[s + i * 9 + j]] += 1;
+					value[theSudoku.guess[s + i * 9 + j]] += 1
 				}
 			}
-			return value.slice(1);
+			return value.slice(1)
 		}
 
 		function get_column(n) {
-			var value = Array(10).fill(0);
-			var s = Math.floor(s / 3) * 9 + (n % 3) * 3;
+			var value = Array(10).fill(0)
+			var s = Math.floor(s / 3) * 9 + (n % 3) * 3
 			for (i = 0; i < 3; i++) {
 				for (j = 0; j < 3; j++) {
-					value[theSudoku.guess[s + i * 9 + j * 3]] += 1;
+					value[theSudoku.guess[s + i * 9 + j * 3]] += 1
 				}
 			}
-			return value.slice(1);
+			return value.slice(1)
 		}
 
 		function get_square(n) {
-			var value = Array(10).fill(0);
+			var value = Array(10).fill(0)
 			for (i = 0; i < 9; i++) {
-				value[theSudoku.guess[n * 9 + i]] += 1;
+				value[theSudoku.guess[n * 9 + i]] += 1
 			}
-			return value.slice(1);
+			return value.slice(1)
 		}
 
 		function found9(n) {
-			var count = 0;
+			var count = 0
 			for (i = 0; i < 81; i++) {
-				if (theSudoku.guess[i] == n) count += 1;
+				if (theSudoku.guess[i] == n) count += 1
 			}
-			return count == 9;
+			return count == 9
 		}
 
 
 		function is_bad(v) {
-			return v.find(n => n > 1) >= 0;
+			return v.find(n => n > 1) >= 0
 		}
 
 		function check_guess() {
-			theSudoku.solved = false;
-			$('#solved').disable(true);
+			theSudoku.solved = false
+			$('#solved').disable(true)
 
-			var counts = Array(10).fill(0);
+			var counts = Array(10).fill(0)
 			for (i = 0; i < 81; i++) {
-				counts[theSudoku.guess[i]]++;
+				counts[theSudoku.guess[i]]++
 			}
 			for (i = 1; i < counts.length; i++) {
 				$("#d" + i).css("color", counts[i] > 8 ? "red" : "")
 			}
 			for (i = 1; i < counts.length; i++) {
-				if (counts[i] != 9) return;
+				if (counts[i] != 9) return
 			}
 			for (var i = 0; i < 9; i++) {
 				if (is_bad(get_row(i)) || is_bad(get_column(i)) || is_bad(get_square(i)))
-					return;
+					return
 			}
-			theSudoku.solved = true;
-			$('#solved').disable(false);
+			theSudoku.solved = true
+			$('#solved').disable(false)
 		}
 
 		function select_row(e) {
-			var name = e.currentTarget.title;
+			var name = e.currentTarget.title
 			try {
-				var q = localStorage["sk " + name].split(';');
-				theSudoku.name = name;
-				theSudoku.value = from_string(q[0]);
-				theSudoku.guess = q.length < 2 ? from_string(q[0]) : from_string(q[1]);
-				theSudoku.solved = q[0].endsWith('Y');
+				var q = localStorage["sk " + name].split(';')
+				theSudoku.name = name
+				theSudoku.value = from_string(q[0])
+				theSudoku.guess = q.length < 2 ? from_string(q[0]) : from_string(q[1])
+				theSudoku.solved = q[0].endsWith('Y')
 			} catch (e) {
-				return;
+				return
 			}
-			title_t.value = theSudoku.name;
+			title_t.value = theSudoku.name
 			change_digit(null)
 			setMode("play")
 		}
 
 		function build_list() {
-			var keys = [];
+			var keys = []
 			for (var i = 0; i < localStorage.length; i++) {
 				var s = localStorage.key(i);
-				if (s.startsWith("sk "))
-					keys.push(s);
+				if (s.startsWith("sk ")) keys.push(s)
 			}
-			$("tr:gt(0)").remove();
-			keys = keys.sort();
+			$("tr:gt(0)").remove()
+			keys = keys.sort()
 			for (var i = keys.length; i > 0; --i) {
-				var s = keys[i - 1];
-				var value = localStorage[s].split(";")[0];
-				var name = s.substring(3);
-				var solved = value.endsWith("Y") ? "&check;" : "";
+				var s = keys[i - 1]
+				var value = localStorage[s].split(";")[0]
+				var name = s.substring(3)
+				var solved = value.endsWith("Y") ? "&check;" : ""
 				$("#items").append('<tr title="' + name + '"><td>' //
 					+ solved + '</td><td>' //
 					+ name + '</td><td>' //
 					+ value + '</td></tr>' //
 				)
-				$("tr:last").click(select_row);
-				if (hide && solved) {
-					$("tr:last").hide();
-				}
+				$("tr:last").click(select_row)
+				if (hide && solved) $("tr:last").hide()
 			}
 		}
 
@@ -324,10 +321,10 @@ $(document).ready(
 			} else {
 				change_digit(li.target)
 			}
-		});
+		})
 		$("#choices > li").each(function (i) {
 			this.id = "d" + this.innerText
-		});
+		})
 
 		// Put the selected digit in the clicked cell
 		$("div > div").click(function (div) {
@@ -339,60 +336,61 @@ $(document).ready(
 				if (undo.length == 1) $("#undo").disable(false)
 				save()
 			}
-		});
+		})
 
 		// Clear all the cells
-		$("div > div").text('');
+		$("div > div").text('')
 
 		// Assign an id for each cell 1-81 
 		$("div > div").each(function (i) { this.id = i })
 
 		// Global actions
 		$('#new').click(function (e) {
-			var today = new Date();
-			today.setMinutes(today.getMinutes() - today.getTimezoneOffset());
-			theSudoku.name = "STrib " + today.toISOString().slice(0, 10);
-			theSudoku.value = empty.slice();
-			theSudoku.guess = empty.slice();
+			var today = new Date()
+			today.setMinutes(today.getMinutes() - today.getTimezoneOffset())
+			theSudoku.name = "STrib " + today.toISOString().slice(0, 10)
+			theSudoku.value = empty.slice()
+			theSudoku.guess = empty.slice()
+			theSudoku.solved = false
 
-			title_t.value = theSudoku.name;
+			title_t.value = theSudoku.name
 
-			setMode("edit");
-		});
+			setMode("edit")
+		})
 
 		// Edit mode actions
 		$("#solve").click(function (e) {
 			if (theSudoku.name != title_t.value) {
 				if (theSudoku.name != "" && localStorage["sk " + theSudoku.name] == value) {
-					delete localStorage["sk " + theSudoku.name];
+					delete localStorage["sk " + theSudoku.name]
 				}
-				theSudoku.name = title_t.value;
+				theSudoku.name = title_t.value
 			}
-			store();
-			setMode("play");
-		});
+			store()
+			setMode("play")
+		})
 		$("#delete").click(function (e) {
-			delete localStorage["sk " + theSudoku.name];
-			this.disabled = true;
-		});
+			delete localStorage["sk " + theSudoku.name]
+			this.disabled = true
+		})
 
-		$("#title").keyup(check_title);
-		$("#inctitle").click(e => inc_title(1));
-		$("#dectitle").click(e => inc_title(-1));
+		$("#title").keyup(check_title)
+		$("#inctitle").click(e => inc_title(1))
+		$("#dectitle").click(e => inc_title(-1))
 
 		// Play mode actions
 		$("#solved").click(function (e) {
-			store();
-			setMode("list");
-		});
+			store()
+			setMode("list")
+		})
 		$("#clear").click(function (e) {
-			change_digit(null);
-			theSudoku.guess = theSudoku.value.slice();
-			setMode("play");
-		});
-		$("#edit").click(e => setMode("edit"));
-		$("#list").click(e => setMode("list"));
-		$("#save").click(e => store());
+			change_digit(null)
+			theSudoku.guess = theSudoku.value.slice()
+			setMode("play")
+		})
+		$("#edit").click(e => setMode("edit"))
+		$("#list").click(e => setMode("list"))
+		$("#save").click(e => store())
 
 		$("#undo").click(function (e) {
 			if (undo.length == 0) return
@@ -402,10 +400,10 @@ $(document).ready(
 		})
 
 		// List panel actions
-		$("#hide").click(e => show_hide(true));
-		$("#show").click(e => show_hide(false));
+		$("#hide").click(e => show_hide(true))
+		$("#show").click(e => show_hide(false))
 
-		restoreSudoku();
+		restoreSudoku()
 
 		// Initialize quip from URL query if present
 		if (document.URL.indexOf("?") > 0) {
